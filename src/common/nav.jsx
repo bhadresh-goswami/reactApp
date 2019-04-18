5849aabdc8a58508240388f92b5571f3983d6f26
bhadresh <gosai09@gmail.com>
2019-04-19 03:19:43 +0530

    new change
    done

--- src/common/nav.jsx
+++ src/common/nav.jsx
@@ -4,50 +4,50 @@ class Nav extends Component {
   state = {};
   render() {
     return (
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <button
-              type="button"
-              className="navbar-toggle collapsed"
-              data-toggle="collapse"
-              data-target="#bs-example-navbar-collapse-1"
-            >
-              <span className="sr-only">Toggle navigation</span> Menu{" "}
-              <i className="fa fa-bars" />
-            </button>
-            <a className="navbar-brand page-scroll" href="#page-top">
-              Start Bootstrap
-            </a>
-          </div>
-
-          <div
-            className="collapse navbar-collapse"
-            id="bs-example-navbar-collapse-1"
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
           >
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                <a className="page-scroll" href="#about">
-                  About
-                </a>
-              </li>
-              <li>
-                <a className="page-scroll" href="#services">
-                  Services
-                </a>
-              </li>
-              <li>
-                <a className="page-scroll" href="#portfolio">
-                  Portfolio
-                </a>
-              </li>
-              <li>
-                <a className="page-scroll" href="#contact">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
+            <span className="sr-only">Toggle navigation</span> Menu{" "}
+            <i className="fa fa-bars" />
+          </button>
+          <a className="navbar-brand page-scroll" href="#page-top">
+            Start Bootstrap
+          </a>
         </div>
+
+        <div
+          className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1"
+        >
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a className="page-scroll" href="#about">
+                About Us
+              </a>
+            </li>
+            <li>
+              <a className="page-scroll" href="#services">
+                Services
+              </a>
+            </li>
+            <li>
+              <a className="page-scroll" href="#portfolio">
+                Portfolio
+              </a>
+            </li>
+            <li>
+              <a className="page-scroll" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     );
   }
 }
